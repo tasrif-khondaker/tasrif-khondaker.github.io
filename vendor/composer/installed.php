ec618008e86408d11db24aca207a14dcf897abec
Khondaker Tasrif Noor <tasrif.khondaker@gmail.com>
2022-10-19 12:40:12 +1100

    wordpress

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve',
+        'pretty_version' => 'v3.4.3',
+        'version' => '3.4.3.0',
+        'reference' => 'e59dc35e7f4f80a1ee4004dfbfea801f11e856b4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve' => array(
+            'pretty_version' => 'v3.4.3',
+            'version' => '3.4.3.0',
+            'reference' => 'e59dc35e7f4f80a1ee4004dfbfea801f11e856b4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.2.30',
+            'version' => '3.2.30.0',
+            'reference' => '7239104ae452b13cd6d506d44e8fc127232d35ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'v1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => '780e564bf0554c419ef447c2b5b768b497ecee40',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
